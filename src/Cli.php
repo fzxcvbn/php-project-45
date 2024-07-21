9436bb0b0635727cdb3b7a59b4008696cbfcadb6
fzxcvbn <diankahid@gmail.com>
2024-07-21 21:24:39 +0300

    finish

--- src/Cli.php
+++ src/Cli.php
@@ -2,13 +2,13 @@
 
 namespace welcome;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-
 use function cli\line;
 use function cli\prompt;
 
 function ask_name()
 {
+    require_once(__DIR__ . '/../vendor/autoload.php');
+
     global $name;
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
