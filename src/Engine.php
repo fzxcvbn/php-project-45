3af9d473d4967114f273fdb803ff61e203e51e0e
fzxcvbn <diankahid@gmail.com>
2024-07-31 23:21:21 +0300

    пробуем исправлять ошибки

--- src/Engine.php
+++ src/Engine.php
@@ -10,18 +10,17 @@ function welcome()
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
+
 function yourAnswer()
 {
     global $name;
     global $answer;
     global $yourAnswer;
-    for ($g = 0; $g < 1; $g++) {
-        $yourAnswer = prompt('Your answer');
-        if ($answer == $yourAnswer) {
-            line('Correct!');
-        } else {
-            line("{$yourAnswer} is wrong answer ;(. Correct answer was {$answer}.");
-            line("Let's try again, %s!", $name);
-        }
+    $yourAnswer = prompt('Your answer');
+    if ($answer == $yourAnswer) {
+        line('Correct!');
+    } else {
+        line("{$yourAnswer} is wrong answer ;(. Correct answer was {$answer}.");
+        line("Let's try again, %s!", $name);
     }
 }
