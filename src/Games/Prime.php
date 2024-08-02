a79dffcff8fa26673dd2492b3af72d167432c01c
fzxcvbn <diankahid@gmail.com>
2024-08-02 23:27:14 +0300

    Молюсь чтобы прошла проверка

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,12 +10,13 @@ function primeGame()
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
     $primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
     $countGames = 3;
+    $answer = '';
     $arrayAnswer = [];
     $arrayQuestion = [];
     for ($i = 0; $i < $countGames; $i++) {
         $question = rand(0, 100);
-        foreach ($primeNums as $index => $primeNum) {
-            if ($primeNums[$index] == $question) {
+        foreach ($primeNums as $primeNum) {
+            if ($primeNum == $question) {
                 $answer = 'yes';
                 break;
             } else {
