e51b3d2472a4998d3c0c646587fd00b20b7cdf9d
fzxcvbn <diankahid@gmail.com>
2024-08-12 18:09:54 +0300

    Исправила ошибку с типом данных

--- src/Engine.php
+++ src/Engine.php
@@ -6,7 +6,7 @@ use function cli\prompt;
 /**
 *@param array<mixed> $Answers;
 *@param array<mixed> $Questions;
-*@param int $welcomeQuestion;
+*@param string $welcomeQuestion;
 */
 
 function launch($Answers, $Questions, $welcomeQuestion)
